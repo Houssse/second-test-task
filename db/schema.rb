64879c8ee81e53e4f33a702e4d1b93cfdda60f73
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2025-03-01 19:04:51 +0300

    create model article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_01_154604) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_01_155807) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
@@ -23,4 +32,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_01_154604) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "articles", "users"
 end
