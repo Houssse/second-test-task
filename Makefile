526c8b5a8bd05554a9e05fdf71b147b5d8100b63
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2025-03-04 23:20:20 +0300

    add test controller

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
 lint:
 	bundle exec rubocop
 	bundle exec slim-lint app/views
+
+test: prepare_test_db
+	bin/rails test
+
+prepare_test_db:
+	bin/rails db:test:prepare
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -3,7 +3,43 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @admin = users(:two)
+    @article = articles(:one)
+
+    sign_in @admin
+  end
+
+  test 'should create article' do
+    params = {
+      article: {
+        title: 'Test Article',
+        body: 'This is a test article.' }
+    }
+
+    post(articles_path, params:)
+
+    new_article = Article.find_by(title: 'Test Article')
+    assert new_article
+  end
+
+  test 'should update article' do
+    params = {
+      article: {
+        title: 'Updated Article',
+        body: 'This is an updated article.' }
+    }
+
+    patch(article_path(@article), params:)
+
+    updated_article = Article.find(@article.id)
+    assert_equal 'Updated Article', updated_article.title
+  end
+
+  test 'should destroy article' do
+    delete(article_path(@article))
+
+    assert_nil Article.find_by(id: @article.id)
+  end
 end
