7ba46362a060c13de53371d0ac5e105430ef4324
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2025-03-05 00:18:45 +0300

    add validate article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,7 @@
 
 class Article < ApplicationRecord
   belongs_to :user
+
+  validates :title, length: { minimum: 3, maximum: 255 }, presence: true
+  validates :body,length:{ minimum: 200 } ,presence: true
 end
